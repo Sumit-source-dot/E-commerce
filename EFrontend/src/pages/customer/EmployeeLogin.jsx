a9829987a93bb45913106b9448ab268d41cb0e82
Sumit-source-dot <modisumit470@gmail.com>
2025-08-08 12:54:26 +0530

    Update EmployeeLogin.jsx

--- EFrontend/src/pages/customer/EmployeeLogin.jsx
+++ EFrontend/src/pages/customer/EmployeeLogin.jsx
@@ -14,7 +14,7 @@ const handleLogin = async (e) => {
   setError("");
 
   try {
-    const res =await axios.post("/api/employee/login", {
+    const res =await axios.post("https://e-commerce-omega-two-88.vercel.app/employee/login", {
   email,
   password,
 });
