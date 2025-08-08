4eb5f0814257ae85275b154c0e97f1c38933b969
Sumit-source-dot <modisumit470@gmail.com>
2025-08-08 13:00:46 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,39 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import morgan from "morgan";
-// import cookieParser from "cookie-parser";
-// import connectDB from "./config/db.js";
-// import adminRoutes from "./routes/adminRoutes.js";
-// import employeeRoutes from "./routes/employeeRoutes.js";
-// import orderRoutes from './routes/orderRoutes.js';
-// import customerRoutes from "./routes/customerRoutes.js";
-
-// dotenv.config();
-// const app = express();
-
-// // Connect DB
-// connectDB();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(morgan("dev"));
-
-// // Routes
-// app.use("/api/admin", adminRoutes);
-// app.use("/api/employee", employeeRoutes);
-// app.use('/api/orders', orderRoutes);
-// app.use("/api/customers", customerRoutes);
-
-// // Server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`✅ Server running on port ${PORT}`);
-// });
-
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
